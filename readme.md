f84c16a225d39550c36526d15aecd6a1aa301a55
peti <dragonpeti53@gmail.com>
2025-11-14 15:53:22 +0200

    0.2.4 preparations, doc update, serve::file asynchronized

--- readme.md
+++ readme.md
@@ -1,1 +1,27 @@
-Zep is a minimal, super simple, async HTTP library in Rust
+Zep is a minimal, super simple, async HTTP library in Rust.
+
+It's designed to be minimal and easy to use, while still being very fast.
+
+Only dependency is `tokio`.
+
+# Example
+
+This code returns a 200 OK response with the body `Hello world!` when GET / is requested.
+
+```
+use zep::{tokio, Router, Request, Response, Server, Method};
+
+async fn root(_req: Request) -> Response {
+    Response::ok("Hello world!")
+}
+
+#[tokio::main]
+async fn main() {
+    let mut router = Router::new();
+    router.route(Method::GET, "/", root);
+    let server = Server::new("0.0.0.0:8080", router);
+    let _ = server.run().await;
+}
+```
+
+Check out our [docs](https://docs.rs/zep/latest/zep/)!
--- src/serve.rs
+++ src/serve.rs
@@ -1,18 +1,18 @@
 //! This is a helper module for convenience to easily serve different types of content over HTTP.
 
 use crate::Response;
-use std::fs;
+use tokio::fs;
 
 /// Returns a 200 OK response with the contents of file located at `path`.
-/// Returns a 500 Internal Server Error response if file could not be read.
-/// Returns a 404 Not Found response if file at `path` does not exist or could not be found.
-pub fn file(path: &str) -> Response {
-    match fs::exists(path) {
-        Ok(true) => match fs::read(path) {
+/// Returns a 500 Internal Server Error response if file could not be read or found.
+/// Returns a 404 Not Found response if file at `path` does not exist.
+pub async fn file(path: &str) -> Response {
+    match fs::try_exists(path).await {
+        Ok(true) => match fs::read(path).await {
             Ok(contents) => Response::ok(contents),
             Err(_) => Response::error(),
         },
         Ok(false) => Response::not_found(),
-        Err(_) => Response::not_found(),
+        Err(_) => Response::error(),
     }
 }
